42e8ab6559c3c70f62520aa69337947f6fb4474d
yobany <yobanydesign@gmail.com>
2020-05-26 16:41:42 -0400

    Nav scrolling has been added.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,28 @@
 console.log("JS link successful!");
+
+let navHome = document.getElementById("nav-home")
+let navSkills = document.getElementById("nav-skills")
+let navProjects = document.getElementById("nav-projects")
+let navContact = document.getElementById("nav-contact")
+
+let scrollHome = document.getElementById("intro-div")
+let scrollSkills = document.getElementById("skills-title")
+let scrollProjects = document.getElementById("projects-title")
+let scrollContact = document.getElementById("contact-title")
+
+navHome.addEventListener("click", function(event) {
+    event.preventDefault()
+    window.scrollTo(0,scrollHome.offsetTop - 80)
+})
+navSkills.addEventListener("click", function(event) {
+    event.preventDefault()
+    window.scrollTo(0,scrollSkills.offsetTop - 80)
+})
+navProjects.addEventListener("click", function(event) {
+    event.preventDefault()
+    window.scrollTo(0,scrollProjects.offsetTop - 80)
+})
+navContact.addEventListener("click", function(event) {
+    event.preventDefault()
+    window.scrollTo(0,scrollContact.offsetTop - 80)
+})
